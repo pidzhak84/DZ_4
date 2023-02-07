25d5e0bf10a6aafd7a443daaf19dc6566f12dc93
pidzhak <pidzhak84@gmail.com>
2023-02-07 21:53:04 +0300

    Ex_27

---
+++ Ex_27/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.WriteLine("Введите число");
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{num1} -> {GetSum(num1)}");
+
+int GetSum(int num)
+{
+    int res = 0;
+    res += num % 10;
+    num /= 10;
+    if (num > 0)
+    {
+        res += GetSum(num);
+    }
+    return res;
+}
